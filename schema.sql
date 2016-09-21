38d736b488646f32e94a8d12050d26de593f98c7
Matt Molnar <matthew.molnar@gmail.com>
2016-09-20 22:41:19 -0400

    User perms and import script

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
-SET SCHEMA = 'public';
+#sudo -u postgres psql
+#
+
+CREATE DATABASE breadth;
+
+
+SET SCHEMA 'public';
 
 CREATE TABLE domain_staging (
-  id      SERIAL PRIMARY KEY,
-  domain  VARCHAR(259)
+  "id"      SERIAL PRIMARY KEY,
+  "domain"  VARCHAR(259)
 );
+GRANT ALL ON domain_staging TO "importer";
--- zone_file_import.sh
+++ zone_file_import.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+file=$1;
+filepath=$(realpath file);
+
+psql \
+  --username=importer
+  --dbname=breadth <<EOF
+COPY domain_staging("domain") FROM '${filepath}' TEXT DELIMETER '\t';
+EOF
