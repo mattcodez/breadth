09612ca619fab5dc293be9dc05056ad0bb04b5f5
Matt Molnar <matthew.molnar@gmail.com>
2016-09-21 20:32:06 -0400

    Import script

--- zone_file_import.sh
+++ zone_file_import.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo usage: $0 tsv-path
+  exit
+fi
+
 file=$1;
 filepath=$(realpath file);
 
 psql \
-  --username=importer
+  --username=importer \
   --dbname=breadth <<EOF
 COPY domain_staging("domain") FROM '${filepath}' TEXT DELIMETER '\t';
 EOF
