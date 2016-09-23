ac525376243d55dbf55a642979f895314a1b14d8
Matt Molnar <matthew.molnar@gmail.com>
2016-09-23 17:30:31 -0400

    Import SQL

--- schema.sql
+++ schema.sql
@@ -10,4 +10,20 @@ CREATE TABLE domain_staging (
   "id"      SERIAL PRIMARY KEY,
   "domain"  VARCHAR(259)
 );
-GRANT ALL ON domain_staging TO "importer";
+--GRANT ALL ON domain_staging TO "importer";
+
+CREATE TABLE domains (
+  "id"      SERIAL PRIMARY KEY,
+  "domain"  VARCHAR(259) UNIQUE NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION add_domains_from_staging()
+RETURNS int AS $$
+BEGIN
+  INSERT INTO domains(domain)
+  SELECT domain FROM domain_staging
+  ON CONFLICT DO NOTHING;
+  GET DIAGNOSTICS insertcount = ROW_COUNT;
+  RETURNING insertcount;
+END;
+$$ LANGUAGE plpgsql;
