3a1e38afda5e86a625d15106d2320e0476e5849f
Matt Molnar <matthew.molnar@gmail.com>
2016-09-26 22:36:34 -0400

    Working on DB connection

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-request("https://www.reddit.com", function(error, response, body) {
-  if(error) {
-    console.error("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
-
-  var $ = cheerio.load(body);
-  console.log($('body').text().substring(100,150));
-
-  // $('div#siteTable > div.link').each(function( index ) {
-  //   var title = $(this).find('p.title > a.title').text().trim();
-  //   var score = $(this).find('div.score.unvoted').text().trim();
-  //   var user = $(this).find('a.author').text().trim();
-  //   console.log("Title: " + title);
-  //   console.log("Score: " + score);
-  //   console.log("User: " + user);
-  // });
-
+var pg = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: 'knex,public'
 });
+console.log(process.env.PG_CONNECTION_STRING);
+pg.select('domain')
+.from('domains')
+.whereBetween('id', [400, 425])
+.then(rows => console.dir(rows));
+
+// request("https://www.reddit.com", function(error, response, body) {
+//   if(error) {
+//     console.error("Error: " + error);
+//   }
+//   console.log("Status code: " + response.statusCode);
+//
+//   var $ = cheerio.load(body);
+//   console.log($('body').text().substring(100,150));
+//
+//
+// });
